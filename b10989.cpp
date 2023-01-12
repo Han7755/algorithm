0df441932bcb8db7055291a68c892c931c5b02db
Han7755 <gksmfgyrud@naver.com>
2023-01-12 11:10:15 +0900

    10989번 해결

--- b10989.cpp
+++ b10989.cpp
@@ -3,4 +3,23 @@ using namespace std;
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    const int arrSize = 10001;
+    int n;
+    cin >> n;
+    int input[arrSize]{0};
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        input[temp]++;
+    }
+    for (int i = 0; i < arrSize; i++)
+        for (int j = input[i]; j > 0; j--)
+        {
+            cout << i << "\n";
+        }
 }
